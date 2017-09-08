50246dd0657a035b74e02799ad6e4b09871aa170
Piotr Kuźmiak <piotr.kuzmiak@bzwbk.pl>
2017-09-08 15:50:44 +0200

    dodanie nowego True do dataframe

--- reports.py
+++ reports.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 from excel_report import Excel_report
 
-df = pd.DataFrame([[129175,'sprzedaz',True, False],[118158,'sprzedaz', False, False],[129175,'sprzedaz',False,True]], columns=['skp','kampania', 'rejestr', 'biling'])
+df = pd.DataFrame([[129175,'sprzedaz',True, False],[118158,'sprzedaz', False, False],[129175,'sprzedaz',True,True]], columns=['skp','kampania', 'rejestr', 'biling'])
 df.loc[:,['rejestr', 'biling']] = df[['rejestr', 'biling']].astype(np.int)
 excel = Excel_report(dataframe=df)
 excel.unload('D:/CRM/wymiana/klamczuszek.xlsx','Raport',groupby=['skp', 'kampania'])
